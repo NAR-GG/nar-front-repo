807d0b93876d4aa7592b7e5bc530684b99717594
hov-i <dbsghdql55555@gmail.com>
2025-12-26 14:09:09 +0900

    feat: sitemap, robots 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,11 +9,51 @@ import {
 import { combinationsQueries } from "@/entities/combinations/model/combinations.queries";
 
 export const metadata: Metadata = {
-  title: "NAR.GG - LOL 프로 경기 챔피언 조합 분석",
+  title: "나르지지 NAR.GG - LOL 프로 경기 챔피언 조합 분석",
   description:
-    "LCK 프로 경기의 챔피언 조합, 1v1 매치업, 승률 통계를 분석합니다.",
+    "LCK, LPL, LEC 프로 경기의 챔피언 조합, 1v1 매치업, 승률 통계를 분석합니다. 롤 프로 경기 메타 분석 사이트.",
+  keywords: [
+    "나르지지",
+    "nar.gg",
+    "롤",
+    "리그오브레전드",
+    "프로경기",
+    "LCK",
+    "LPL",
+    "LEC",
+    "챔피언 조합",
+    "매치업",
+    "승률",
+    "메타 분석",
+  ],
   icons: {
     icon: "/images/nar-browser-icon.png",
+  },
+  openGraph: {
+    title: "나르지지 NAR.GG - LOL 프로 경기 챔피언 조합 분석",
+    description:
+      "LCK, LPL, LEC 프로 경기의 챔피언 조합, 1v1 매치업, 승률 통계를 분석합니다.",
+    url: "https://nar.gg",
+    siteName: "나르지지 NAR.GG",
+    locale: "ko_KR",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "나르지지 NAR.GG - LOL 프로 경기 챔피언 조합 분석",
+    description:
+      "LCK, LPL, LEC 프로 경기의 챔피언 조합, 1v1 매치업, 승률 통계를 분석합니다.",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+    },
+  },
+  alternates: {
+    canonical: "https://nar.gg",
   },
 };
 
